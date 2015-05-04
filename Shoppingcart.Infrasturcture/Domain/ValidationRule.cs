c2e3fb051a1648f8830d5088f345310503108d4b
Filip Kraszewski <filip.kraszewski@gmail.com>
2015-05-04 13:31:47 +0200

    First push of existing project!

---
+++ Shoppingcart.Infrasturcture/Domain/ValidationRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppingcart.Infrasturcture.Domain
+{
+    public class ValidationRule
+    {
+        private string _property;
+        public string _rule;
+
+        public ValidationRule(string property, string rule) 
+        {
+            _property = property;
+            _rule = rule;
+        }
+
+        public string Property
+        {
+            get { return _property; }
+            set { _property = value; }
+        }
+
+        public string ru
+    }
+}
