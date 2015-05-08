d4b8da4c71e43971f1e433c149e57c52fbcc8b39
Filipeiro <filip.kraszewski@gmail.com>
2015-05-08 12:41:59 +0200

    Part02 fix

--- Shoppingcart.Infrasturcture/Configuration/WebConfigApplicationSettings.cs
+++ Shoppingcart.Infrasturcture/Configuration/WebConfigApplicationSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,6 @@ namespace Shoppingcart.Infrasturcture.Domain.Configuration
 {
     public class WebConfigApplicationSettings : IApplicationSettings
     {
-        string LoggerName { get; }
+        public string LoggerName { get{ return ConfigurationManager.AppSettings["LoggerName"]}; }
     }
 }
