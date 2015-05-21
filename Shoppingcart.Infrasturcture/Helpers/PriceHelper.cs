6399655d9dbcd77ce8e96b02229f39252fc44b73
Filipeiro <filip.kraszewski@gmail.com>
2015-05-21 11:56:42 +0200

    Shopping Cart Application Part-04 (Email Settings)

---
+++ Shoppingcart.Infrasturcture/Helpers/PriceHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppingcart.Infrasturcture.Helpers
+{
+    public static class PriceHelper
+    {
+        public static string FormatMoney(this decimal price)
+        {
+            return String.Format("BDT{0}", price);
+        }
+    }
+}
